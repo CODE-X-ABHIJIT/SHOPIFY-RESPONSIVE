b8f4dca2ef61b386674bb1da65b663e4bdcfda1b
CODE-X-ABHIJIT <codexabhijit@gmail.com>
2024-08-26 22:08:10 +0530

    new commit

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const dbgr = require('debug')("development:mongoose");
+const mongoose=require('mongoose');
+const config=require('config');
+const dbgr=require('debug')("development:mongoose");
 
-// Connection string should be pulled from your configuration
-const connectionString = `${config.get("MONGODB_URI")}/shopify`;
-
-mongoose.connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
+.then(function(){
+    dbgr("connected");
+    
 })
-.then(() => dbgr("Connected to MongoDB"))
-.catch((err) => dbgr("Error connecting to MongoDB:", err));
+.catch(function(err){
+    dbgr(err);
+    
+})
 
-module.exports = mongoose.connection;
+module.exports=mongoose.connection;
